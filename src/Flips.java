62ce8d87300a23ae410ea8eeaf22c5d2f780cec3
Jay Patel <jay037493@gmail.com>
2023-11-29 11:14:48 +0530

    algs4 jar added

--- src/Flips.java
+++ src/Flips.java
@@ -1,8 +1,8 @@
-import org.w3c.dom.css.Counter;
+import edu.princeton.cs.algs4.Counter;
 
 public class Flips {
     public static void main(String[] args) {
         int T = Integer.parseInt(args[0]);
-
+        Counter heads = new Counter("heads");
     }
 }
