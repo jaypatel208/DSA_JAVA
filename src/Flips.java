de19bd0921c0ae270317ed27b6fff30fb280e482
Jay Patel <jay.dev@tech.mekanism.cc>
2023-11-29 11:13:35 +0530

    algs4 jar added

---
+++ src/Flips.java
@@ -0,0 +1,8 @@
+import org.w3c.dom.css.Counter;
+
+public class Flips {
+    public static void main(String[] args) {
+        int T = Integer.parseInt(args[0]);
+
+    }
+}
--- src/RandomSeqBook.java
+++ src/RandomSeqBook.java
@@ -1,3 +1,6 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
 public class RandomSeqBook
 {
     public static void main(String[] args)
