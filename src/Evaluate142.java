840dda2255ab5e3248d9630b1bd3d2532a0c30df
Jay Patel <jay037493@gmail.com>
2023-12-11 11:01:43 +0530

    dijkstra algorithm done

--- src/Evaluate142.java
+++ src/Evaluate142.java
@@ -1,41 +1,35 @@
 import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 
 import java.util.Stack;
 
+//Dijkstra’s Two-Stack Algorithm for Expression Evaluation
 public class Evaluate142 {
     public static void main(String[] args) {
         Stack<String> ops = new Stack<>();
         Stack<Double> vals = new Stack<>();
         while (!StdIn.isEmpty()) {
             String s = StdIn.readString();
-            if (s.equals("(")) {
-
-            } else if (s.equals("+")) {
-                ops.push(s);
-            } else if (s.equals("-")) {
-                ops.push(s);
-            } else if (s.equals("*")) {
-                ops.push(s);
-            } else if (s.equals("/")) {
-                ops.push(s);
-            } else if (s.equals("sqrt")) {
-                ops.push(s);
-            } else if (s.equals(")")) {
-                String op = ops.pop();
-                double v = vals.pop();
-                if (op.equals("+")) {
-                    v = vals.pop() + v;
-                } else if (op.equals("-")) {
-                    v = vals.pop() - v;
-                } else if (op.equals("*")) {
-                    v = vals.pop() * v;
-                } else if (op.equals("/")) {
-                    v = vals.pop() / v;
-                } else if (op.equals("sqrt")) {
-                    v = Math.sqrt(v);
+            switch (s) {
+                case "(" -> {
                 }
-                vals.push(v);
+                case "+", "-", "*", "/", "sqrt" -> ops.push(s);
+                case ")" -> {
+                    String op = ops.pop();
+                    double v = vals.pop();
+                    v = switch (op) {
+                        case "+" -> vals.pop() + v;
+                        case "-" -> vals.pop() - v;
+                        case "*" -> vals.pop() * v;
+                        case "/" -> vals.pop() / v;
+                        case "sqrt" -> Math.sqrt(v);
+                        default -> v;
+                    };
+                    vals.push(v);
+                }
+                default -> vals.push(Double.parseDouble(s));
             }
         }
+        StdOut.println(vals.pop());
     }
 }
